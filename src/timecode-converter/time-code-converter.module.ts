787fae6fbcf720603849c9be54081606a1660660
renauddev <racinet.renaud@orange.Fr>
2022-03-24 14:07:50 +0100

    Feat(TimeCodeConverterModule): Add @Nestjs/throttler and limit 100 requests per minute
    package.json: Add package
    TimeCodeConverterModule: import module

--- src/timecode-converter/time-code-converter.module.ts
+++ src/timecode-converter/time-code-converter.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TimeCodeConverterService } from './time-code-converter.service';
 import { TimeCodeConverterController } from './time-code-converter.controller';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
+  imports: [
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 100,
+      ignoreUserAgents: [/googlebot/gi, /bingbot/gi],
+    }),
+  ],
   controllers: [TimeCodeConverterController],
   providers: [TimeCodeConverterService],
 })
