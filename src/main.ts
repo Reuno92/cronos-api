eb70280590fb0ba67f678340410c2eaa26c03230
renauddev <racinet.renaud@orange.Fr>
2022-03-24 14:39:57 +0100

    Feat(TimeCodeConverterService): Add helmet and cors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import helmet from 'helmet';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const corsOptions = {
+    origin: '*',
+    methods: 'GET,OPTIONS',
+    maxAge: 3600,
+  };
+
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: corsOptions,
+  });
+
+  app.use(helmet());
+
   await app.listen(7500);
 }
 bootstrap();
