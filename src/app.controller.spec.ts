7a249635fb2d9f690cd21ab10a7c963db80d25aa
renauddev <racinet.renaud@orange.Fr>
2022-03-25 16:49:08 +0100

    test(AppController): change test root

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -16,7 +16,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+      expect(appController.getStart()).toBe('<h1>Server is online</h1>');
     });
   });
 });
